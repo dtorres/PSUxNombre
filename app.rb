acb8b1ccccb94ff62907e76bf5dce752b3d2de6f
Diego Torres <warorface@gmail.com>
2012-12-25 21:08:29 -0300

    Initial commit

---
+++ app.rb
@@ -0,0 +1,5 @@
+require "sinatra"
+
+get "/" do
+	"<html><head><title>Resultados PSU por nombre</title></head><body><h1>Resultados PSU 2013</h1><p>PRONTO, DESPUES DEL 3 DE ENERO</p></body></html>"
+end
