9acb081996117c23c1066cbf9f9492c7670f5ea1
Diego Torres <warorface@gmail.com>
2013-01-03 01:35:10 -0300

    RESULTADOS!

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 require "sinatra"
+require "mysql2"
+
+@@dbClient = Mysql2::Client.new({:database => "psu", :username => "root", :password => "", :host => "localhost", :reconnect => true})
 
 get "/resultados" do
-	"Not Yet"
+    
+    params.each do |key, value|
+        val = value.upcase
+        val = val.gsub(/%/,"")
+        val = val.gsub(/[ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž]/,"?")
+        params[key] = val
+    end
+    nombre = "%" + params[:nombres] + "%" + params[:apellido_pat] + "%" + params[:apellido_mat] + "%"
+    query = "SELECT * FROM respsu_2013 WHERE nombre like '#{nombre}' AND (mat > 0 OR le > 0) LIMIT 0, 50"
+    puts query
+    results = @@dbClient.query(query);
+    
+	erb :resultados, :locals => {"results" => results}
 end
 
 get "/" do 
